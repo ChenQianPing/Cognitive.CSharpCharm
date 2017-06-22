aa37effa55ebccfc3cba98c3141bbced32a2d43a
chenqp <13542427@qq.com>
2017-06-22 10:05:22 +0800

    first commit

--- Cognitive.CSharpCharm/Program.cs
+++ Cognitive.CSharpCharm/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Cognitive.CSharpCharm.Algorithm;
 using Cognitive.CSharpCharm.Patterns.FactoryPattern.Factory02;
+using Cognitive.CSharpCharm.Utils;
 
 namespace Cognitive.CSharpCharm
 {
@@ -17,7 +18,12 @@ namespace Cognitive.CSharpCharm
             // A01.A0101();
             // A01.A0102();
 
-            A03.A0301();
+            // A03.A0301();
+
+            var result = U01.GenerateScoreDetail(5.0);
+
+            Console.WriteLine(result);
+            Console.Read();
 
             /*
             var sw = new Stopwatch();
---
+++ Cognitive.CSharpCharm/Utils/U01.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cognitive.CSharpCharm.Utils
+{
+    public static class U01
+    {
+        /// <summary>
+        /// 生成分数明细；
+        /// </summary>
+        /// <param name="score"></param>
+        /// <returns></returns>
+        public static string GenerateScoreDetail(double score)
+        {
+            var scoreDetail = "";
+
+            for (double i = 0; i <= score; i = i + 0.5)
+            {
+                if (Math.Abs(i) <= 0)
+                {
+                    scoreDetail = scoreDetail + i.ToString(CultureInfo.InvariantCulture);
+                }
+                else
+                {
+                    scoreDetail = scoreDetail + "," + i.ToString(CultureInfo.InvariantCulture);
+                }
+                
+            }
+
+            return scoreDetail;
+        }
+    }
+}
